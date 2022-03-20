6ef9af1a19c4d04da3345d734d26355f9fa32788
Jonathan Moser <jonathancmoser@gmail.com>
2022-03-20 13:47:55 -0400

    added thought

---
+++ models/Thought.js
@@ -0,0 +1,37 @@
+const moment = require('moment');
+const { Schema, model } = require('mongoose');
+
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: 'Enter your thought',
+      minlength: [1, 'Needs to be between 1 and 280 characters'],
+      maxlength: [280, 'Needs to be between 1 and 280 characters']
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    },
+    username: {
+      type: String,
+      required: 'Please enter your username'
+    },
+    reactions: []
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    }
+  }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+  return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought; 
