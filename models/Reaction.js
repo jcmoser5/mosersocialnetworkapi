0e3259e58a5ea2cbd3e1ce518de16d2002328128
Jonathan Moser <jonathancmoser@gmail.com>
2022-03-20 13:49:42 -0400

    added reaction

---
+++ models/Reaction.js
@@ -0,0 +1,32 @@
+const moment = require('moment');
+const { Schema, model, Types } = require('mongoose');
+
+const ReactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      required: 'Enter a reaction',
+      maxlength: [280, 'Needs to be less than 280 characters']
+    },
+    username: {
+      type: String,
+      required: 'Enter your username'
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    }
+  },
+  {
+    toJSON: {
+      getters: true
+    }
+  }
+);
+
+module.exports = ReactionSchema; 
