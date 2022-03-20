9c3ac638ca7a42b39e7bf6b912aca7433b1a7b98
Jonathan Moser <jonathancmoser@gmail.com>
2022-03-20 14:18:35 -0400

    added methods to thought routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,3 +1,35 @@
 const router = require('express').Router();
+const {
+  getAllThoughts,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  deleteReaction
+} = require('../../controllers/thought-controller');
+
+router
+  .route('/')
+  .get(getAllThoughts)
+  .post(createThought);
+
+  router
+  .route('/:thoughtId')
+  .get(getThoughtById)
+  .put(updateThought);
+
+  router
+  .route('/:userId/:thoughtId')
+  .delete(deleteThought);
+
+  router
+  .route('/:thoughtId/reactions')
+  .post(createReaction);
+
+  router
+  .route('/:thoughtId/reactions/:reactionId')
+  .delete(deleteReaction);
+
 
 module.exports = router; 
